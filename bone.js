30399f65545e36b72f289f43596ed0396ffdb70d
bparrish206 <brentparrish@Rosias-MacBook-Pro.local>
2014-12-14 11:44:17 -0800

    starting profile

--- bone.js
+++ bone.js
@@ -1,3 +1,5 @@
+Dogs = new Meteor.Collection('dogs');
+
 if (Meteor.isClient) {
     angular.module('bone',['angular-meteor']);
 
@@ -5,7 +7,7 @@ if (Meteor.isClient) {
       angular.bootstrap(document, ['bone']);
     });
 
-    angular.module("bone").controller("profileCtrl", ['$scope', function($scope){
+      angular.module("bone").controller("profileCtrl", ['$scope', function($scope){
       $scope.photoUpload = function() {
         var preview = document.querySelector('img'); //selects the query named img
         var file    = document.querySelector('input[type=file]').files[0]; //sames as here
@@ -17,6 +19,7 @@ if (Meteor.isClient) {
 
         if (file) {
           reader.readAsDataURL(file); //reads the data as a URL
+          Dogs.insert({name: $scope.Dogs.name, age: $scope.Dogs.age, sex: $scope.Dogs.sex, breed: $scope.Dogs.breed}); //img: reader.readAsDataURL(file)});
         } else {
           preview.src = "";
         }
