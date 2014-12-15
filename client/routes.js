910d29b06fb6e50f0213cdb5f0098725586901b6
Paul Laskowski <pelaskowski@gmail.com>
2014-12-15 11:14:02 -0800

    reorganized code

---
+++ client/routes.js
@@ -0,0 +1,14 @@
+angular.module("boneApp").config(['$urlRouterProvider', '$stateProvider', '$locationProvider',
+  function($urlRouterProvider, $stateProvider, $locationProvider) {
+
+  $locationProvider.html5Mode(true);
+
+  $stateProvider
+    .state('main', {
+      url: '/main',
+      template: UiRouter.template('landing-view.html'),
+      controller: 'LandingCtrl'
+    });
+
+    $urlRouterProvider.otherwise('/main');
+}]);
