a3e751671de22d2ef433bcc0f89f89c0815f4a80
bparrish206 <brentparrish@Rosias-MacBook-Pro.local>
2014-12-14 17:52:51 -0800

    fixed bug

--- client/profiles/controllers/profileCtrl.js
+++ client/profiles/controllers/profileCtrl.js
@@ -1,4 +1,6 @@
 angular.module("bone").controller("profileCtrl", ['$scope', function($scope){
+
+
   $scope.photoUpload = function() {
     var preview = document.querySelector('img'); //selects the query named img
     var file    = document.querySelector('input[type=file]').files[0]; //sames as here
@@ -6,13 +8,20 @@ angular.module("bone").controller("profileCtrl", ['$scope', function($scope){
 
     reader.onloadend = function () {
       preview.src = reader.result;
-    };
+      };
 
-    if (file) {
-      reader.readAsDataURL(file); //reads the data as a URL
-      Dogs.insert({name: $scope.Dogs.name, age: $scope.Dogs.age, sex: $scope.Dogs.sex, breed: $scope.Dogs.breed}); //img: reader.readAsDataURL(file)});
-    } else {
-      preview.src = "";
-    }
-  };
+      if (file) {
+        reader.readAsDataURL(file); //reads the data as a URL
+        Dogs.insert({
+          name: $scope.Dogs.name,
+          bio: $scope.Dogs.description,
+          photo: reader.result,
+          age: $scope.Dogs.age,
+          sex: $scope.Dogs.sex,
+          breed: $scope.Dogs.breed,
+        });
+      } else {
+        preview.src = "";
+      }
+    };
 }]);
