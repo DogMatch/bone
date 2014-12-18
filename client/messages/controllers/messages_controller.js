11957d0a1d5f53cbf4094c616b057406d2b00fe7
Paul Laskowski <pelaskowski@gmail.com>
2014-12-18 12:43:34 -0800

    start of matches list under messages route

--- client/messages/controllers/messages_controller.js
+++ client/messages/controllers/messages_controller.js
@@ -1,3 +1,14 @@
-angular.module('boneApp').controller('MessageCtrl', ['$scope', '$location', function($scope, $location) {
+angular.module('boneApp').controller('MessagesCtrl', ['$scope', '$location', function($scope, $location) {
+  if (!Meteor.userId()) {
+    $location.path('/');
+  } else {
+    $scope.curUserId = Meteor.userId();
+  }
+  
+  $scope.matchedDogs = Dogs.find({matches: $scope.curUserId}).fetch()
+  ;
+  console.log($scope.matchedDogs);
+  
+  
   
 }]);
