41e7c7c8c175c3a7f73a2d8810b1ff1264497a0e
Paul Laskowski <pelaskowski@gmail.com>
2014-12-17 20:17:06 -0800

    fixed some minor route problems

--- client/routes.js
+++ client/routes.js
@@ -14,7 +14,7 @@ angular.module('boneApp').config(['$urlRouterProvider', '$stateProvider', '$loca
       template: UiRouter.template("dog-profile-view.html"),
       controller: "DogProfileCtrl"
     })
-    .state('match', {
+    .state('matchup', {
       url: "/matchup",
       template: UiRouter.template("match-view.html"),
       controller: "MatchCtrl"
