62dcdf2a672f23703f07d4cbf87f1c5778f67ec8
Paul Laskowski <pelaskowski@gmail.com>
2015-01-03 01:21:43 -0800

    fixed query selector

--- client/messages/controllers/messages_controller.js
+++ client/messages/controllers/messages_controller.js
@@ -5,7 +5,7 @@ angular.module('boneApp').controller('MessagesCtrl', ['$scope', '$rootScope', '$
   $scope.errors = [];
   var sub = Meteor.subscribe('matchedDogProfiles');
   Tracker.autorun(function(self) {
-    $scope.matchedDogs = Dogs.find({}).fetch();
+    $scope.matchedDogs = Dogs.find({user_id: {$ne: Meteor.userId()}}).fetch();
     if (!$scope.$root.$$phase) $scope.$apply();
     $scope.$on('$destroy', function () {
       self.stop(); // Stop computation if scope is destroyed.
