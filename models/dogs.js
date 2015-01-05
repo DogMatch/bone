ebd3950cbe67431b19eea3a5d03be7b62e03af50
Paul Laskowski <pelaskowski@gmail.com>
2015-01-04 16:50:15 -0800

    adding crud permissions on dogs model, refactored dog_profile_controller to comply

--- models/dogs.js
+++ models/dogs.js
@@ -1,1 +1,37 @@
 Dogs = new Meteor.Collection('dogs');/* jshint ignore:line */
+
+Dogs.allow({  
+  insert: function (userId, doc) {
+    return userId;
+  },
+  update: function (userId, doc, fields, modifier) {
+    // allow updating your own dog profile
+    if (doc.user_id === userId) return true;
+
+    // allow adding/removing your userId in these properties
+    if (modifier.$addToSet.upVotes === userId) return true;
+    if (modifier.$unset.upVotes === userId) return true;
+    if (modifier.$addToSet.downVotes === userId) return true;
+    if (modifier.$addToSet.matches === userId) return true;
+    if (modifier.$unset.matches === userId) return true;
+    return false;
+  },
+  remove: function (userId, doc) {
+    // can only remove your own documents
+    return doc.user_id === userId;
+  }
+});
+
+Dogs.deny({
+  update: function (userId, doc, fields, modifier) {
+    if (doc.user_id === userId && !fields.user_id) {
+      // don't deny updating own dog profile, but deny updating user_id
+      return false;
+    } else if (_.without(fields, ['upVotes', 'downVotes', 'matches'])) {
+      // deny updating properties other than these on other dog profiles
+      return true;
+    } else {
+      return false;
+    }
+  }
+});
